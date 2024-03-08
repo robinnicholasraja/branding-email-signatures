447ee978ceb2948cae709f845d92792b93df3f33
robinnicholas <robin.raja@anywhere.co>
2024-03-08 19:59:04 +0530

    feat: minor fixes

--- src/app/wr/page.tsx
+++ src/app/wr/page.tsx
@@ -206,7 +206,7 @@ const WR = () => {
                   onClick={handleShowCodeBlock}
                   className="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold mt-4 text-white"
                 >
-                  Show codeblock
+                  {showCodeBlock ? "Hide" : "Show"} codeblock
                 </button>
               </div>
               <div className="mt-5">
