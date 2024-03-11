65b682489e730b6161f4cfc6b3e4d331e5eb2c29
robinnicholas <robin.raja@anywhere.co>
2024-03-11 11:24:46 +0530

    fix: minor fix in codeblock

--- src/app/wr/CodeBlock.tsx
+++ src/app/wr/CodeBlock.tsx
@@ -16,27 +16,26 @@ export function CodeBlock(props: CodeBlockProps) {
   const handleClick = () => {
     setCopy(true);
     setTimeout(() => {
-      setCopy(false)
+      setCopy(false);
     }, 700);
     const text = prettiedMarkup;
-    if (window.clipboardData && window.clipboardData.setData) {
-      // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
-      return clipboardData.setData("Text", text);
-    }
-    if (
-      document.queryCommandSupported &&
-      document.queryCommandSupported("copy")
-    ) {
+
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(text).catch((err) => {
+        console.warn("Copy to clipboard failed.", err);
+      });
+    } else {
+      // Fallback for browsers that do not support the Clipboard API
       const textarea = document.createElement("textarea");
       textarea.textContent = text;
-      textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in Microsoft Edge.
+      textarea.style.position = "fixed";
       document.body.appendChild(textarea);
       textarea.select();
+
       try {
-        return document.execCommand("copy"); // Security exception may be thrown by some browsers.
+        document.execCommand("copy");
       } catch (ex) {
         console.warn("Copy to clipboard failed.", ex);
-        return false;
       } finally {
         document.body.removeChild(textarea);
       }
@@ -53,9 +52,7 @@ export function CodeBlock(props: CodeBlockProps) {
         className="bg-sky-500 hover:bg-sky-700 px-5 py-2 text-sm leading-5 rounded-full font-semibold mt-4 text-white absolute right-4 top-4 flex items-center gap-2"
         onClick={handleClick}
       >
-        <span>
-          {copy ? 'Copied' : 'Copy'}
-        </span>
+        <span>{copy ? "Copied" : "Copy"}</span>
       </button>
       <SyntaxHighlighter language="html" style={prism} showLineNumbers={true}>
         {prettiedMarkup}
